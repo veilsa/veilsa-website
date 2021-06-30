89aa7ffa897f49ef1d58f5028aa734a99c395eae
Yolster <yolster@protonmail.com>
2021-06-30 16:24:41 +0300

    created aboutus

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home - Veilsa' });
 });
 
+router.get('/aboutus', function(req, res, next) {
+  res.render('aboutus', { title: 'Hakkımızda - Veilsa' });
+});
+
+
 module.exports = router;
